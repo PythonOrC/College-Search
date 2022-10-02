f859d8293261f93e02d9cdfa9da9575917f584d6
Ethan Hu <ethanhu8351@gmail.com>
2022-10-02 01:16:04 -0700

    finished

---
+++ Search.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import sys
+
+
+class Search:
+    def __init__(self, file_addr):
+        self.COLLEGE_URL_PATTERN = re.compile(r"^/college\/.*")
+        self.PAGE = "&page="
+        self.results = set()
+        self.import_file(file_addr)
+        self.search_college()
+        self.save_list()
+
+    def import_file(self, file_addr):
+        with open(file_addr, "r") as file:
+            self.urls = [line.strip() for line in file]
+
+    def search_college(self):
+        if self.urls == []:
+            return
+        current_url = self.urls.pop()
+        print("Searching: " + current_url)
+        response = requests.get(current_url)
+        self.content = BeautifulSoup(response.text, "html.parser")
+
+        college_url = self.content.find_all(
+            "a", href=self.COLLEGE_URL_PATTERN, class_=None
+        )
+        [
+            self.results.add(college.text) if college.text.replace("\n", "").strip() else None
+            for college in college_url
+        ]
+        self.construct_url(current_url)
+        if self.has_next():
+            self.urls.append(self.next_page_url)
+        self.search_college()
+
+    def has_next(self):
+        return self.content.find("a", href=self.next_page_pattern)
+
+    def construct_url(self, url):
+        base_url, page = url.split(self.PAGE) if self.PAGE in url else (url, "1")
+        self.next_page_url = base_url + self.PAGE + str(int(page) + 1)
+        self.next_page_pattern = "/" + "/".join(self.next_page_url.split("/")[-2:])
+
+    def save_list(self):
+        with open("colleges.txt", "w", encoding="UTF-8") as file:
+            for result in self.results:
+                print(result)
+                file.write(result + "\n")
+
+
+if __name__ == "__main__":
+    # if len(sys.argv) < 2:
+    #     print("Usage: python3 Search.py <file>")
+    #     sys.exit(1)
+    # else:
+    #     Search(sys.argv[1])
+
+    Search("url.txt")
+
